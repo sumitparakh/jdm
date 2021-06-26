184b9de366210f241b30be9f5a142c17d533eca3
Sumit Parakh <sumit.thecoder@gmail.com>
2021-06-26 20:40:24 +0530

    initialize app

---
+++ src/main/java/com/jdm/app/App.java
@@ -0,0 +1,13 @@
+package com.jdm.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static String text = "Hello World!";
+
+    public static void main(String[] args) {
+        System.out.println(text);
+    }
+}
