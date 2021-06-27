9fbf0c66fa0074786e6bdf2b4bb0e3a3c4a8799b
Sumit Parakh <sumit.thecoder@gmail.com>
2021-06-27 18:03:21 +0530

    feat(core): log4j dependency configured
    Closes #2

--- src/main/java/com/jdm/app/App.java
+++ src/main/java/com/jdm/app/App.java
@@ -1,13 +1,19 @@
 package com.jdm.app;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static String text = "Hello World!";
+    public static final Logger LOG = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
         System.out.println(text);
+        LOG.log(Level.INFO, "log4j configured");
     }
 }
